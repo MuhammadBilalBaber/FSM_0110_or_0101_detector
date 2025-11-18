380ab187a33b7f22ba1c61d0bdcf794f5bdf7802
MuhammadBilalBaber <bilaljutt345a@gmail.com>
2025-11-18 16:41:41 +0500

    added fsm.tcl to test  using jaspergold tool

---
+++ fsm.tcl
@@ -0,0 +1,16 @@
+clear -all
+
+analyze -sv12 fsm.sv  
+
+elaborate -top pattern_0110_or_0101_detector -create_related_covers {precondition witness}
+
+clock clk
+reset reset
+
+##Deadlock, live_lock, and deadcode verification
+check_superlint -init
+config_rtlds -rule -enable -tag { FSM_IS_DLCK FSM_IS_LLCK }
+check_superlint -extract
+
+# command to run
+# jg fsm.tcl
